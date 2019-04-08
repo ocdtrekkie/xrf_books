a389a5c57082d6be26cb5dcf15bae141a20ee260
Jacob Weisz <inbox@jacobweisz.com>
2019-04-08 15:10:58 -0500

    EVG display should default to shelf view

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ echo "<a href=\"search_results.php?sort=recent&limit=100&view=shelf\">Recently A
 {
 echo "<a href=\"search_results.php?filter=EB\">Electronic Books</a><br>
 <a href=\"search_results.php?filter=EPER\">Electronic Magazines</a><br>
-<a href=\"search_results.php?filter=EVG\">Electronic Games</a><br>
+<a href=\"search_results.php?filter=EVG&view=shelf\">Electronic Games</a><br>
 <a href=\"search_results.php?filter=ESD\">Electronic Software</a><p>";
 } ?>
 
