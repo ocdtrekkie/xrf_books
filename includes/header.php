f2f0d74237c026b7c402e9cded238ee905f39661
Jacob Weisz <inbox@jacobweisz.com>
2020-06-17 11:29:54 -0500

    Populate library configuration values

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,13 @@
 <?php
+
+$xrfl_config_query="SELECT * FROM l_config";
+$xrfl_config_result=mysqli_query($xrf_db, $xrfl_config_query);
+
+$xrfl_library_home=xrf_mysql_result($xrfl_config_result,0,"library_home");
+$xrfl_library_barcode=xrf_mysql_result($xrfl_config_result,0,"library_barcode");
+$xrfl_library_local_repository=xrf_mysql_result($xrfl_config_result,0,"library_local_repository");
+$xrfl_library_remote_mailto=xrf_mysql_result($xrfl_config_result,0,"library_remote_mailto");
+
 header('Content-Type: text/html; charset=iso-8859-15');
 if ($xrf_mystylepref == "") {$xrf_style = $xrf_style_default;}
 else {$xrf_style = $xrf_mystylepref;}
