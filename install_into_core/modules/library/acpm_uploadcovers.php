c7e04fc8f4ae358ab30bece3ef7d34dcb3ebaad1
Jacob Weisz <inbox@jacobweisz.com>
2020-07-01 12:09:14 -0500

    Add initial cover uploader

---
+++ install_into_core/modules/library/acpm_uploadcovers.php
@@ -0,0 +1,91 @@
+<?php
+/* Credit to W. S. Toh https://code-boxx.com/simple-drag-and-drop-file-upload/ */
+/* MIT License */
+
+require("ismodule.php");
+require("modules/$modfolder/functions_lib.php");
+require("modules/$modfolder/include_lconfig.php");
+$do = $_GET['do'];
+if ($do == "upload")
+{
+	// SOURCE + DESTINATION
+	$source = $_FILES["file-upload"]["tmp_name"];
+	$destination = "../books/covers/" . $_FILES["file-upload"]["name"];
+	$error = "";
+
+	// CHECK IF FILE ALREADY EXIST
+	if (file_exists($destination)) {
+	  $error = $destination . " already exist.";
+	}
+
+	// ALLOWED FILE EXTENSIONS
+	if ($error == "") {
+	  $allowed = ["jpg", "jpeg", "png", "gif"];
+	  $ext = strtolower(pathinfo($_FILES["file-upload"]["name"], PATHINFO_EXTENSION));
+	  if (!in_array($ext, $allowed)) {
+		$error = "$ext file type not allowed - " . $_FILES["file-upload"]["name"];
+	  }
+	}
+
+	// LEGIT IMAGE FILE CHECK
+	if ($error == "") {
+	  if (getimagesize($_FILES["file-upload"]["tmp_name"]) == false) {
+		$error = $_FILES["file-upload"]["name"] . " is not a valid image file.";
+	  }
+	}
+
+	// FILE SIZE CHECK
+	if ($error == "") {
+	  // 1,000,000 = 1MB
+	  if ($_FILES["file-upload"]["size"] > 50000000) {
+		$error = $_FILES["file-upload"]["name"] . " - file size too big!";
+	  }
+	}
+
+	// ALL CHECKS OK - MOVE FILE
+	if ($error == "") {
+	  if (!move_uploaded_file($source, $destination)) {
+		$error = "Error moving $source to $destination";
+	  }
+	}
+
+	// ERROR OCCURED OR OK?
+	if ($error == "") {
+	  echo $_FILES["file-upload"]["name"] . " upload OK";
+	} else {
+	  echo $error;
+	}
+}
+else
+{
+	echo "<style>
+      #uploader {
+        width: 300px; 
+        height: 200px; 
+        background: #0f0f0f;
+        padding: 10px;
+      }
+      #uploader.highlight {
+        background:#ff0;
+      }
+      #uploader.disabled {
+        background:#aaa;
+      }
+    </style>
+    <script src=\"modules/$modfolder/acpm_uploadcovers.js\"></script>
+	
+	<!-- DROP ZONE -->
+    <div id=\"uploader\">
+      Drop Files Here
+    </div>
+
+    <!-- STATUS -->
+    <div id=\"upstat\"></div>
+
+    <!-- FALLBACK -->
+    <form action=\"acp_module_panel.php?modfolder=$modfolder&modpanel=uploadcovers&do=upload\" method=\"post\" enctype=\"multipart/form-data\">
+      <input type=\"file\" name=\"file-upload\" id=\"file-upload\" accept=\"image/*\">
+      <input type=\"submit\" value=\"Upload File\" name=\"submit\">
+    </form>";
+}
+?>
