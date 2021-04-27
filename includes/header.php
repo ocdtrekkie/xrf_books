0984727aa0f475ac92c7aa6cb22c068cd85968f1
Jacob Weisz <inbox@jacobweisz.com>
2021-04-27 12:40:24 -0500

    Implement print style

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@ else {$xrf_style = $xrf_mystylepref;}
 if ($xrf_page_subtitle != "") { $xrf_title_nugget = " - "; }
 echo "<html><head><title>$xrf_site_name Library$xrf_title_nugget$xrf_page_subtitle</title>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"styles/$xrf_style/style.css\" />
+<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/print/style.css\" media=\"print\" />
 </head><body>";
 
 if ($xrf_myid != 0)
@@ -24,10 +25,10 @@ echo "<div class=\"header\" align=\"center\">
 <table width=\"100%\"><tr><td>
 <p align=\"left\">
 <font size=\"6\" color=\"white\">$xrf_site_name Library</font><br>
-<font color=\"white\"><a href=\"$xrf_site_url\"><font color=\"white\">$xrf_myusername</font></a></font>
+<font color=\"white\" class=\"navigation-box\"><a href=\"$xrf_site_url\"><font color=\"white\">$xrf_myusername</font></a></font>
 </p>
 </td><td>
-<p align=\"right\">
+<p align=\"right\" class=\"navigation-box\">
 <font color=\"white\">$navbox$sstatus</font>
 </p>
 </td></tr></table>
--- viewrecord.php
+++ viewrecord.php
@@ -102,7 +102,7 @@ if(mysqli_num_rows($result) != 0){
 
 if ($xrf_myulevel > 1) {
 
-echo "<p align=\"left\"><b>Actions:</b> <font size=\"2\"><a href=\"add_to_reading_list.php?passid=$smallcode\">[Add to Reading List]</a> ";
+echo "<p align=\"left\" class=\"actions-bar\"><b>Actions:</b> <font size=\"2\"><a href=\"add_to_reading_list.php?passid=$smallcode\">[Add to Reading List]</a> ";
 
 if ($xrf_myulevel > 3) {
 	if (file_exists("acp_module_panel.php")) {
